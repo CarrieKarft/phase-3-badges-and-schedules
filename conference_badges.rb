bbfa4e7558c6f47d220216e5c378c2390c087295
Carrie Kraft <carriekarft@gmail.com>
2023-07-11 20:50:21 -0700

    commit tree 96e7c14e5003bdfcf381d8c2e473ec2806015f30

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+require "pry"
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(array) 
+    array.map.each_with_index do |name, index|
+        "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    end
+end
+
+def printer(array)
+    batch_badge_creator(array).each do |item|
+        puts item
+    end
+    assign_rooms(array).each do |item|
+        puts item
+    end
+end
+
+binding.pry 0
